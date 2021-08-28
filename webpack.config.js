ffc2e8a17536447a0739142b48e734ca21a02700
Pawan Kumar <pawanyadav1501@gmail.com>
2021-08-29 00:08:18 +0530

    Change port

--- webpack.config.js
+++ webpack.config.js
@@ -250,7 +250,7 @@ module.exports = {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     host: '0.0.0.0',
-    port: 9000,
+    port: 443,
     historyApiFallback: true,
   },
   externals: {
